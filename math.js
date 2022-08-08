4ab01476f865650af6ac3448d8aad20ced59e4c6
hafizurrahmanrazib <hafizur201611044@gmail.com>
2022-08-08 09:53:50 +0600

    math properties exploration done

---
+++ math.js
@@ -0,0 +1,51 @@
+// Math.pow
+/* const result = Math.pow(3, 8);
+console.log(result); */
+
+// Math.abs
+/* const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+if (gap < 5) {
+    console.log('You Guys Can Be Friends');
+}
+else {
+    console.log('You Guys Stay Apart');
+} */
+
+// Math.round
+/* const number = 2.4598;
+const fullNumber = Math.round(number);
+console.log(fullNumber);
+ */
+
+//Math.ceil
+/* const number = 2.0001;
+const fullNumber = Math.ceil(number);
+console.log(fullNumber); */
+
+//Math.floor
+/* const number = 2.999;
+const fullNumber = Math.floor(number);
+console.log(fullNumber); */
+
+// Random
+/* console.log(Math.random());
+console.log(Math.random() * 100); */
+
+// Round And Random
+/* for (let i = 0; i < 20; i++) {
+    const random = Math.round(Math.random() * 6);
+    console.log(random);
+} */
+
+// To Get Maximum Number
+// console.log(Math.max(2, 3, 6, 43, 67, 87));
+
+// To Get minimum Number
+// console.log(Math.min(2, 3, 6, 43, 67, 87));
+
+// To Get Value Of PI
+console.log(Math.PI);
+
